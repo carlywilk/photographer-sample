57ad41a25b39db8aeb5aadb8e3f48221f4cfe2e1
Carly Wilk <carlymwilk@gmail.com>
2024-09-03 02:11:38 -0400

    Start styling header; backup for old styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,29 @@ import "./Header.scss";
 
 export function Header() {
     return (
-        <div>
-        <h1 class="header__name">Ella Parker</h1>
-        <nav>
-            <Link className="header__link" to="/">
-                <h2 class="header__button">Home</h2>
-            </Link>
-            <Link className="header__link" to="/about">
-                <h2 class="header__button">About</h2>
-            </Link>
-            <Link className="header__link" to="/series">
-                <h2 class="header__button">Series</h2>
-            </Link>
-            <Link className="header__link" to="/contact">
-                <h2 class="header__button">Contact</h2>
-            </Link>
-        </nav>
+        <div className="header">
+            {/* <div className="header__title">
+                <div className="header__name">
+                    <h1 className="header__first-name">Ella</h1>
+                    <h1 className="header__last-name">Parker</h1>
+                </div>
+                <h1 className="header__job">Photography</h1>
+            </div> */}
+            <h1 className="header__title">Ella Parker Photography</h1>
+            <nav className="header__nav">
+                <Link className="header__link" to="/">
+                    <h2 className="header__button">Home</h2>
+                </Link>
+                <Link className="header__link" to="/about">
+                    <h2 className="header__button">About</h2>
+                </Link>
+                <Link className="header__link" to="/series">
+                    <h2 className="header__button">Series</h2>
+                </Link>
+                <Link className="header__link" to="/contact">
+                    <h2 className="header__button">Contact</h2>
+                </Link>
+            </nav>
         </div>
     )
 }
