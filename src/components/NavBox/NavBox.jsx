acdfde60e39c5edb722f3bcb41791708dff51dec
Carly Wilk <carlymwilk@gmail.com>
2024-09-04 15:30:39 -0400

    Add images for each series and style landing page

--- src/components/NavBox/NavBox.jsx
+++ src/components/NavBox/NavBox.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 
+import igIcon from "../../assets/icons/instagram.svg";
+import linkedInIcon from "../../assets/icons/linkedin.svg";
+import emailIcon from "../../assets/icons/email.svg";
+
 import "./NavBox.scss";
 
 export function NavBox() {
     return (
         <div className="navbox">
-            <h1 className="navbox__title">Photographer Site</h1>
+            <h1 className="navbox__title">Photographer</h1>
             <nav className="navbox__menu">
                 <Link className="navbox__link" to="/">
                     <p className="navbox__button">Home</p>
@@ -17,6 +21,17 @@ export function NavBox() {
                     <p className="navbox__button">Contact</p>
                 </Link>
             </nav>
+            <div className="navbox__socials">
+                <a className="navbox__link" href="https://www.instagram.com/">
+                    <img className="navbox__icon" src={igIcon} alt="" />
+                </a>
+                <a className="navbox__link" href="https://www.linkedin.com/in/carly-wilk/">
+                    <img className="navbox__icon" src={linkedInIcon} alt="" />
+                </a>
+                <a className="navbox__link" href={`mailto:carly.wilk.dev@gmail.com`}>
+                    <img className="navbox__icon" src={emailIcon} alt="" />
+                </a>
+            </div>
         </div>
     )
 }
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,6 +2,11 @@
 import { NavBox } from "../../components/NavBox/NavBox";
 
 import exampleImage from "../../assets/images/test-image.jpeg";
+import architectureImage from "../../assets/images/architecture.jpg";
+import concertsImage from "../../assets/images/concerts.jpg";
+import ediblesImage from "../../assets/images/edibles.jpg";
+import natureImage from "../../assets/images/nature.jpg";
+import portraitsImage from "../../assets/images/portraits.jpg";
 
 import "./Landing.scss";
 
@@ -17,22 +22,27 @@ export function Landing() {
                     <NavBox />
                 </div>
                 <div className="landing__box">
-                    <p>2</p>
+                    {/* <p>2</p> */}
+                    <img className="landing__image" src={portraitsImage} alt=""/>
                 </div>
                 <div className="landing__box">
-                    <p>3</p>
+                    {/* <p>3</p> */}
+                    <img className="landing__image--resized" src={natureImage} alt=""/>
                 </div>
             </div>
 
             <div className="landing__row--lower">
                 <div className="landing__box">
-                    <p>4</p>
+                    {/* <p>4</p> */}
+                    <img className="landing__image--resized" src={concertsImage} alt=""/>
                 </div>
                 <div className="landing__box">
-                    <p>5</p>
+                    {/* <p>5</p> */}
+                    <img className="landing__image--resized" src={ediblesImage} alt=""/>
                 </div>
                 <div className="landing__box">
-                    <p>6</p>
+                    {/* <p>6</p> */}
+                    <img className="landing__image" src={architectureImage} alt=""/>
                 </div>
             </div>
         </div>
