97fc99ff3f0edc6503be532d64874a04ea5cb2d6
Carly Wilk <carlymwilk@gmail.com>
2024-09-18 10:17:46 -0400

    Add content for about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,16 @@
 
+import cameraImage from "../../assets/images/camera.jpg";
+
 import "./About.scss";
 
 export function About() {
     return (
         <>
-            <h1>About page</h1>
+            <h1 className="about__title">About the Photographer</h1>
+            <div className="about__content-container">
+                <img className="about__image" src={cameraImage} alt="" />
+                <p className="about__content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus sit tempore quam maiores cumque itaque cum, nemo totam beatae. Magnam magni officiis qui voluptatem eveniet, autem mollitia aut nulla?</p>
+            </div>
         </>
     )
 }
