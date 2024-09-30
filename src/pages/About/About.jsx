66786eaa197df100f770025464d9fbd7618ad544
Carly Wilk <carlymwilk@gmail.com>
2024-09-30 15:01:50 -0400

    Update about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,7 +8,7 @@ export function About() {
         <>
             <h1 className="about__title">About the Photographer</h1>
             <div className="about__content-container">
-                <img className="about__image" src={cameraImage} alt="" />
+                <img className="about__image" src={cameraImage} alt="about image" />
                 <p className="about__content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus sit tempore quam maiores cumque itaque cum, nemo totam beatae. Magnam magni officiis qui voluptatem eveniet, autem mollitia aut nulla?</p>
             </div>
         </>
